c7ca486f1b6ab7c0a54fe9155cc2d49b7d046110
ohseungmin <ohmin2886@gmail.com>
2023-12-13 14:19:32 +0900

    then과 catch를 사용해서 응답 결과 출력 에러 감지

--- promise-practice.js
+++ promise-practice.js
@@ -6,3 +6,9 @@ let promise = new Promise(function(resolve, reject) {
 console.log(promise)
 
 
+// then을 써서 응답 결과 출력 그리고 catch로 오류 출력
+promise.then(function(resolveData) {
+  console.log(resolveData);
+}).catch( err => {
+  console.error(err);
+} )
